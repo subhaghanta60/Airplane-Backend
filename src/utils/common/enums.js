1ec47a077cb665cb2fc6520239a9a730b7b950b7
Subha Ghanta <subhaghanta60@gmail.com>
2025-04-06 13:23:56 +0530

    Feat: Add Seat Table

---
+++ src/utils/common/enums.js
@@ -0,0 +1,12 @@
+const SEAT_TYPE= {
+    BUSINESS: 'Business',
+    ECONOMY :'economy',
+    PREMIUM_ECONOMY:'premium-economy',
+    FIRST_CLASS:'first_class'
+
+}
+
+
+module.exports = {
+    SEAT_TYPE
+}
